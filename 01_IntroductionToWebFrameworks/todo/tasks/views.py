51ff9d4bc00aa38b6feaaee4077babcbafd5fe45
Artur Ibragimov <Artursrst@gmail.com>
2022-09-01 22:10:57 +0300

    Tasks completed!

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from django.views import View
 
 
+
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
         return HttpResponse('<ul>'
                             '<li>Установить python</li>'
                             '<li>Установить django</li>'
+                            '<li>Выполнить задания</li>'
                             '<li>Запустить сервер</li>'
                             '<li>Порадоваться результату</li>'
                             '</ul>')
