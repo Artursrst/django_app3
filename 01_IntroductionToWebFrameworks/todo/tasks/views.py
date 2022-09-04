7ff5de91221aaa4040b8a13f286f6d3d553af269
Artur Ibragimov <Artursrst@gmail.com>
2022-09-04 13:37:55 +0300

    Tasks completed!

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
-
+import random
 from django.views import View
+
 
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
+
+        tasks = ['task1', 'task2', 'task3', 'task4', 'task5', 'task6', 'task7', 'task8', 'task9', 'task10']
+        tasks_to_do = random.sample(tasks, k=10)
+
         return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Выполнить задания</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+                            '<li>{}</li>'
+                            '<li>{}</li>'
+                            '<li>{}</li>'
+                            '<li>{}</li>'
+                            '<li>{}</li>'
+                            '</ul>'.format(tasks_to_do[0],
+                                           tasks_to_do[1],
+                                           tasks_to_do[2],
+                                           tasks_to_do[3],
+                                           tasks_to_do[4]))
