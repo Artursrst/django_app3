d6952d9ede63b7c9c26a351fee13a25180c0e09e
Артур Ибрагимов <artursrst@gmail.com>
2023-08-21 14:34:43 +0300

    Completed

--- queries/mysite/blogapp/admin.py
+++ queries/mysite/blogapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Article, Author, Category, Tag
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = "title", "content", "pub_date", "author", "category",
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = "name", "bio",
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "name",
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = "name",
