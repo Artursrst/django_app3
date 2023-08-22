fe4fa0cf040d5d634248d2c943dc4c9939f70761
Артур Ибрагимов <artursrst@gmail.com>
2023-08-22 20:16:32 +0300

    Questions

--- queries/mysite/blogapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ArticlesListView
-
-
-app_name = "blogapp"
-
-urlpatterns = [
-    path("articles/", ArticlesListView.as_view(), name="articles-list"),
-]
